e8218816f9294584ead143bcc49b6810761e4bc5
jy-hui <huijy-wm19@student.tarc.edu.my>
2022-04-13 02:37:38 +0800

    By JY

--- app/src/main/java/com/example/donateanything/fragments/DonateForm2Fragment.kt
+++ app/src/main/java/com/example/donateanything/fragments/DonateForm2Fragment.kt
@@ -1,6 +1,11 @@
 package com.example.donateanything.fragments
 
+import android.app.PendingIntent
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.telephony.SmsManager
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,6 +37,9 @@ class DonateForm2Fragment : Fragment() {
         val icNo = argsFrom?.getString("icNo")
         val date = argsFrom?.getString("date")
 
+        var donateID : String = ""
+        var d_ID_F : String = ""
+        var d_ID : Int = 0
         val titleText= view.findViewById<TextView>(R.id.donateItem);
         titleText.setText(title)
         val bankSpinner : Spinner = view.findViewById(R.id.bank_spinner)
@@ -62,20 +70,73 @@ class DonateForm2Fragment : Fragment() {
             }
         }
 
+        requestBtn.setOnClickListener() {
+            //com.example.donateanything.fragments.init(System.getenv("Acount"),System.getenv("Acount_Token"))
+        }
+
+
         val btnSubmit: Button =view.findViewById(R.id.submitBtn)
         db= FirebaseFirestore.getInstance()
+        db.collection("DONATION").get().addOnSuccessListener {
+            if(it.isEmpty){
+                donateID = "0"
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.d(ContentValues.TAG, "get failed with ", exception)
+            }
+        db.collection("DONATION").get().addOnSuccessListener { result ->
+            for (document in result) {
+                donateID = document.id
 
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.d(ContentValues.TAG, "get failed with ", exception)
+            }
         btnSubmit.setOnClickListener(){
+            var pacPassword : Int = 12345
+            pacPassword = pacNo.text.toString().toInt()
+            if(pacPassword == 12345){
+                Log.w(ContentValues.TAG, "Donation :"+ donateID )
+                d_ID = donateID.toInt()
+                d_ID++
+                d_ID_F = d_ID.toString()
+                val donation = hashMapOf(
+                    "ID" to d_ID,
+                    "Email" to bankR,
+                    "NoIC" to icNo,
+                    "Date" to date,
+                    "AccountNo" to accountNo.text.toString(),
+                    "Payment" to payment.text.toString()
+                )
+
+                val Donation = db.collection("DONATION")
+                val query = Donation.get()
+                    .addOnSuccessListener { documentReference ->
+                        Donation.document(d_ID_F).set(donation)
+                        Navigation.findNavController(it).navigate(R.id.action_donateForm2Fragment_to_receiptFragment)
+                    }
+                    .addOnFailureListener { e ->
+
+                    }
+            }else{
+                Toast.makeText(requireActivity().applicationContext,"Pac No Invalid",Toast.LENGTH_SHORT).show()
+            }
 
 
-            //Navigation.findNavController(it).navigate(R.id.action_donateFragment_to_donateForm1Fragment)
+
 
 
         }
 
-        // Inflate the layout for this fragment
         return view
     }
-
+    private fun getShareIntent() : Intent {
+        val shareIntent = Intent(Intent.ACTION_SENDTO)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, "Pac No: 12345")
+        return shareIntent
+    }
 
 }
