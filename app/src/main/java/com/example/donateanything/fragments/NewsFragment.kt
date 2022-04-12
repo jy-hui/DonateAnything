f0906e31dbbf7c19ecc7372effc67ec73881563e
jy-hui <huijy-wm19@student.tarc.edu.my>
2022-04-12 11:28:59 +0800

    By JY

--- app/src/main/java/com/example/donateanything/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/donateanything/fragments/NewsFragment.kt
@@ -1,12 +1,12 @@
 package com.example.donateanything.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.donateanything.R
 
@@ -18,6 +18,10 @@ class NewsFragment : Fragment() {
 
     }
 
+    val isFood:Boolean = true;
+    val isMoney:Boolean = true;
+    val isSupply:Boolean = true;
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,11 +31,24 @@ class NewsFragment : Fragment() {
 
         btnBack.setOnClickListener(){
             Navigation.findNavController(it).navigate(R.id.action_newsFragment_to_homeFragment)
+
         }
+
         val donate1: Button =view.findViewById(R.id.donate1)
 
         donate1.setOnClickListener(){
+
             Navigation.findNavController(it).navigate(R.id.action_newsFragment_to_donateFragment)
+            val bundle = Bundle()
+            bundle.putBoolean("isFoodItem",isFood)
+            bundle.putBoolean("isSupplyItem",isSupply)
+            bundle.putBoolean("isMoneyItem",isMoney)
+            val fragmentItem = DonateFragment()
+            fragmentItem.arguments = bundle
+            fragmentManager?.beginTransaction()?.replace(R.id.container_fragment,fragmentItem)?.commit()
+            //val intent = Intent(requireActivity(),DonateActivity.class)
+
+
         }
 
         val donate2: Button =view.findViewById(R.id.donate2)
