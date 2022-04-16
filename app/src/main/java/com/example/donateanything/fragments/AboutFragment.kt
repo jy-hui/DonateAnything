fc0115b079243364454659f620b03c51a05bbb51
RyanYee1057 <yeezs-wm19@student.tarc.edu.my>
2022-04-16 22:51:25 +0800

    by Zs

--- app/src/main/java/com/example/donateanything/fragments/AboutFragment.kt
+++ app/src/main/java/com/example/donateanything/fragments/AboutFragment.kt
@@ -1,10 +1,13 @@
 package com.example.donateanything.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.donateanything.R
 
 class AboutFragment : Fragment() {
@@ -13,9 +16,44 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val companyPhone : TextView = view.findViewById(R.id.showCContact)
+        val companyEmail : TextView = view.findViewById(R.id.showCEmail)
+        val partnerPhone : TextView = view.findViewById(R.id.showPContact)
+        val partnerEmail : TextView = view.findViewById(R.id.showPEmail)
+
+        companyPhone.setOnClickListener{
+            val intentCP = Intent(Intent.ACTION_DIAL)
+            intentCP.data = Uri.parse("tel: 0123456789")
+            startActivity(intentCP)
+        }
+
+        companyEmail.setOnClickListener{
+            composeEmail("serious4gamers@gmail.com","Hello!")
+        }
+
+        partnerPhone.setOnClickListener{
+            val intentPP = Intent(Intent.ACTION_DIAL)
+            intentPP.data = Uri.parse("tel: 0129876543")
+            startActivity(intentPP)
+        }
+
+        partnerEmail.setOnClickListener{
+            composeEmail("donationforeveryone@gmail.com","Hello!")
+        }
+
+        return view
     }
 
+    private fun composeEmail(address: String, subject: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, address)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, "Send To $address")
+        }
+            startActivity(intent)
+    }
 
 }
